27c90e0d589c87296e389bef8760cc5a99f60064
ShskedEmuna <shaked.emuna@gmail.com>
2023-06-21 09:00:36 +0300

    #1- stopwatch ex

---
+++ index.js
@@ -0,0 +1,22 @@
+const sw = new Stopwatch();
+function Stopwatch() {
+  let starded = false;
+  let time = 0;
+  let inter;
+  this.duration = time;
+  this.start = function () {
+    if (!starded) {
+      starded = true;
+      inter = setInterval(() => {
+        time++;
+        this.duration = time;
+      }, 1000);
+    } else throw new Error("already starded");
+  };
+  this.stop = function () {
+    if (starded) {
+      starded = false;
+      clearInterval(inter);
+    } else throw new Error("already stopped");
+  };
+}
