198138b6b93056f54a22aeef7cdd6f56af758bea
shaharizhak <shaharizhak811@gmail.com>
2023-06-25 14:00:18 +0300

    this is the message

---
+++ stackEx.js
@@ -0,0 +1,34 @@
+const _arr = Symbol();
+const _count = Symbol();
+
+class Stack {
+  constructor() {
+    this[_arr] = [];
+    this[_count] = 0;
+  }
+
+  get count() {
+    return this[_count];
+  }
+
+  push(item) {
+    this[_arr].push(item);
+    this[_count]++;
+  }
+
+  pop() {
+    if (this[_count] > 0) {
+      this[_count]--;
+      this[_arr].pop();
+    } else throw new console.error("empty stack");
+  }
+
+  peek() {
+    if (this[_count] > 0) {
+      return this[_arr][Number(this[_count]) - 1];
+    }
+    throw new Error("empty stack");
+  }
+}
+
+const s = new Stack();
